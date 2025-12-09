55f41058e767ffe51ce07e247a47218cbe13ec35
Diego <diegopgimenes@gmail.com>
2025-12-09 06:50:20 -0300

    q

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -5,6 +5,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // ✅ necessário para enviar o cookie JSESSIONID
 });
 
 export default api;
